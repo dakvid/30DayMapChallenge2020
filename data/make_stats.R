b75ac913888bdda0c174d1c49a9c9c286c123dcf
David Friggens <david@frigge.nz>
2020-12-04 10:40:51 +1300

    Make list of maps for Datawrapper table

--- data/make_stats.R
+++ data/make_stats.R
@@ -1,10 +1,21 @@
 library(magrittr)
 library(readr)
+library(glue)
 library(dplyr)
 library(tidyr)
+library(stringr)
 
+challenges <- read_tsv("data/challenges.tsv")
+
+cartographers <- read_tsv("data/cartographers.tsv", col_types = "ccccc")
+maps <- read_tsv("data/maps.tsv", col_types = "ccccccc")
 classifications <- read_tsv("data/classifications.tsv", col_types = "ccccccc")
+images <- read_tsv("data/images.tsv", col_types = "ccccc")
+
 coords_countries <- read_tsv("data/coords_countries.tsv")
+
+
+# Maps --------------------------------------------------------------------
 
 country_map_data <- 
   classifications %>% 
@@ -20,3 +31,27 @@ country_map_data <-
   )
 
 write_csv(country_map_data, "../30DayMapChallenge2020Metadata/stats/country_map_data.csv")
+
+
+
+# Tables ------------------------------------------------------------------
+
+map_list <- 
+  maps %>% 
+  inner_join(classifications, by = c("handle", "Day")) %>% 
+  inner_join(images, by = c("handle", "Day")) %>% 
+  inner_join(cartographers, by = "handle") %>% 
+  inner_join(challenges, by = "Day") %>% 
+  transmute(Challenge = glue("**{Day}** ^{Challenge}^"),
+            Cartographer = glue("**@{handle}** ^{coalesce(realname,username)}^"),
+            Map = glue("![Map](https://david.frigge.nz/30DayMapChallenge2020/thumbnails/{mapid}.{extension})"),
+            Links = glue("[tweet](https://twitter.com/{handle}/status/{tweet_id}) {if_else(website=='_','',paste0('[web](',website,')'))}"),
+            Area = area %>% str_replace('^_$', '') %>% str_replace_all(',', ', '),
+            City = city %>% str_replace('^_$', '') %>% str_replace_all(',', ', '),
+            Topics = topics %>% str_replace('^_$', '') %>% str_replace_all(',', ', '),
+            Types = types %>% str_replace('^_$', '') %>% str_replace_all(',', ', '),
+            Tools = tools %>% str_replace('^_$', '') %>% str_replace_all(',', ', '))
+
+write_tsv(map_list, "data/map_list_for_table.tsv")
+
+
