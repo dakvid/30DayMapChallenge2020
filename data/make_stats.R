0f8e0a4bfbb71408f0b85deae1e522dd29d73cb1
David Friggens <david@frigge.nz>
2020-12-16 23:01:31 +1300

    Save challenge maps by day stats

--- data/make_stats.R
+++ data/make_stats.R
@@ -13,9 +13,10 @@ classifications <- read_tsv("data/classifications.tsv", col_types = "ccccccc")
 images <- read_tsv("data/images.tsv", col_types = "ccccc")
 
 coords_countries <- read_tsv("data/coords_countries.tsv")
+coords_cities <- read_csv("data/worldcities.csv")
 
 
-# Maps --------------------------------------------------------------------
+# Maps - Countries --------------------------------------------------------------------
 
 country_map_data <- 
   classifications %>% 
@@ -34,19 +35,48 @@ write_csv(country_map_data, "../30DayMapChallenge2020Metadata/stats/country_map_
 
 
 
+# Maps - Cartographer Countries -------------------------------------------
+
+
+
 cartographer_map_data <- 
   cartographers %>% 
-  count(country) %>% 
+  inner_join(maps, by = "handle") %>% 
+  group_by(country) %>% 
+  summarise(`Number of Cartographers` = n_distinct(handle),
+            `Number of Maps` = n()) %>% 
+  ungroup() %>% 
   inner_join(coords_countries,
              by = c(country = "name")) %>% 
   select(
     Lat = latitude,
     Long = longitude,
     Country = country,
-    `Number of Maps` = n
+    `Number of Cartographers`,
+    `Number of Maps`
   )
 
 write_csv(cartographer_map_data, "../30DayMapChallenge2020Metadata/stats/cartographer_map_data.csv")
+
+
+# Maps - Cities -----------------------------------------------------------
+
+city_map_data <- 
+  classifications %>% 
+  filter(city != "_") %>% 
+  inner_join(coords_cities,
+             by = c(area = "country",
+                    "city")) %>% 
+  count(city, area, lat, lng) %>% 
+  select(
+    Lat = lat,
+    Long = lng,
+    City = city,
+    Country = area,
+    `Number of Maps` = n
+  )
+write_csv(city_map_data, "../30DayMapChallenge2020Metadata/stats/city_map_data.csv")
+
 
 # Tables ------------------------------------------------------------------
 
@@ -69,4 +99,21 @@ map_list <-
 
 write_csv(map_list, "data/map_list_for_table.csv")
 
+
+
+# Heatmap -----------------------------------------------------------------
+
+day_matrix <- 
+  maps %>% 
+  transmute(date_utc = date_posted %>% str_sub(1, 10),
+            Day) %>% 
+  count(date_utc, Day) %>% 
+  arrange(Day, date_utc) %>% 
+  pivot_wider(names_from = "Day",
+              values_from = "n") %>%
+  arrange(date_utc) %>% 
+  filter(date_utc > "2020-10-15",
+         date_utc < "2020-12-08")
+
+write_csv(day_matrix, "data/day_matrix_for_heatmap.csv")
 
